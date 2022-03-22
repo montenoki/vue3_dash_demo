8da2ebb41134bf169b6e7abc2c82a0d4da500f63
monten.oki <monten.oki@gmail.com>
2022-03-22 21:19:57 +0900

    chore: import packages

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import locate from './language'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+    .use(router)
+    .use(VueAxios, axios)
+    .use(locate)
+    .mount('#app')
