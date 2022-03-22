d51c35b38dfbff0ed1a84d21e29c9c14e942eb7e
monten.oki <monten.oki@gmail.com>
2022-03-22 23:08:11 +0900

    feat: update VueDash.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory, RouterOptions } from 'vue-router';
 
 const routes = [
-    { path: "/", redirect: "home" },
-    { name: 'home', path: '/home', component: () => import('../components/pages/Home.vue'), meta: { requireAuth: false } },
-    { name: 'NotFound', path: '/page-not-found', component: () => import('../components/pages/PageNotFound.vue'), meta: { requireAuth: false } },
+    { path: "/", redirect: "/dash" },
+    { name: 'home', path: '/dash', component: () => import('../components/VueDash.vue'), meta: { requireAuth: false } },
+    { name: 'NotFound', path: '/page-not-found', component: () => import('../components/PageNotFound.vue'), meta: { requireAuth: false } },
     { path: '/:pathMatch(.*)*', redirect: "/page-not-found" },
 ]
 const router = createRouter({
