1b486a05d4ed90db157fc7facaafa84369bf5c32
monten.oki <monten.oki@gmail.com>
2022-03-24 02:14:19 +0900

    fix: path name

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, RouterOptions } from 'vue-router';
 
 const routes = [
     { path: "/", redirect: "/dash" },
-    { name: 'home', path: '/dash', component: () => import('../components/VueDash.vue'), meta: { requireAuth: false } },
+    { name: 'dash', path: '/dash', component: () => import('../components/VueDash.vue'), meta: { requireAuth: false } },
     { name: 'NotFound', path: '/page-not-found', component: () => import('../components/PageNotFound.vue'), meta: { requireAuth: false } },
     { path: '/:pathMatch(.*)*', redirect: "/page-not-found" },
 ]
