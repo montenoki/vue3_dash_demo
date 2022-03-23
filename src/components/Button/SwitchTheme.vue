670cb418e5bd2aaf06223f5fde2339308b0bc643
monten.oki <monten.oki@gmail.com>
2022-03-24 02:16:06 +0900

    feat: add SwitchTheme button

---
+++ src/components/Button/SwitchTheme.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { mapMutations } from "vuex";
+import { useStore } from "../../store";
+import { defineComponent, computed } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      theme_name: computed(() => useStore().getters.getThemeName),
+    };
+  },
+  methods: {
+    ...mapMutations({
+      updateThemeName: "updateThemeName",
+    }),
+    switchTheme() {
+      const theme_name = this.theme_name === "light" ? "dark" : "light";
+      this.updateThemeName({ theme_name: theme_name });
+    },
+  },
+});
+</script>
+
+<template>
+  <n-button @click="switchTheme">{{ theme_name }}</n-button>
+</template>
